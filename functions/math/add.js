273692112ed23aae20b49eca8ae246e5c923bc0a
cahyatt.dev <calebhyatt4663@gmail.com>
2020-09-26 16:25:01 -0400

    Added add.js

---
+++ functions/math/add.js
@@ -0,0 +1,20 @@
+let _ = function (array) {
+  if (typeof array !== Array) throw new Error("Input must be an array.");
+  else {
+    let sum = 0, acceptedInputs = [ String, Number ];
+
+    for (let i = 0; i < array.length; i++) {
+      if (acceptedInputs.includes(typeof i)) {
+        try {
+          sum += Number(i);
+        } catch(e) {
+          throw new Error(e);
+        }
+      } else throw new Error(`${i} is not a Number or String.`);
+    }
+
+    return sum;
+  }
+};
+
+module.exports = _;
